9f67178d7e4a67a45107e3bae96ba28f759f4ea3
GeraldoTI <sobrinhotecnologia@gmail.com>
2023-09-07 02:03:49 -0300

    full

---
+++ dockerfile
@@ -0,0 +1,9 @@
+# Use a imagem base do NGINX
+FROM nginx
+
+# Copie os arquivos do seu site para o diretório de conteúdo do NGINX
+
+COPY ./index.html /usr/share/nginx/html
+
+
+
---
+++ javascript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#productForm').submit(function(e) {
+        e.preventDefault();
+        alert('Produto cadastrado com sucesso!');
+    });
+});
